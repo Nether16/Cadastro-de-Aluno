301815b9e16284905ab72268693c2afda90d7ee3
Nether <enzonether@gmail.com>
2024-03-08 22:17:18 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Entre com seu nome inteiro : ");
+        string Nome = Console.ReadLine();
+        Console.Write("Entre com sua idade : ");
+        int Idade = int.Parse(Console.ReadLine());
+        Console.Write("Entre com sua nota  : ");
+        int Nota = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"O aluno {Nome} tem {Idade} anos de idade e esta com uma nota de {Nota} pontos");
+    }
+}
